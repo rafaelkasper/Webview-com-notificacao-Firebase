ecccc66e0f6d7a7a174bf2de41e0316669bcbbea
Rafael Kasper <prof.rafaelkasper@gmail.com>
2020-06-10 09:00:19 -0300

    Versão 2.1

---
+++ app/src/main/java/com/rafaelkasper/appruibarbosa/MainActivity.java
@@ -0,0 +1,49 @@
+package com.rafaelkasper.appruibarbosa;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+    Intent intent;
+    TextView aviso;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+        iniciarAplicativo();
+
+
+
+
+        aviso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Obrigado por utilizar nosso App!", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+    private void iniciarAplicativo() {
+        int TIME_SPLASH;
+        TIME_SPLASH = 3 * 1000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                intent = new Intent(MainActivity.this, PrincipalActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        },TIME_SPLASH);
+        aviso = findViewById(R.id.aviso);
+        aviso.setText("Versão : " + BuildConfig.VERSION_NAME);
+    }
+
+}
---
+++ app/src/main/java/com/rafaelkasper/appruibarbosa/PrincipalActivity.java
@@ -0,0 +1,40 @@
+package com.rafaelkasper.appruibarbosa;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
+public class PrincipalActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_principal);
+        webView = findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://emefruibarbosa.xyz/atividades");
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(PrincipalActivity.this, "Atualizando...", Toast.LENGTH_LONG).show();
+                webView.loadUrl("https://emefruibarbosa.xyz/atividades");
+            }
+        });
+    }
+
+
+}
